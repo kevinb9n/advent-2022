14332a968b5a4cbbe85071a4007a9621e917d79f
kevinb9n <kevinb9n@gmail.com>
2022-12-01 21:29:50 -0800

    day 2: got my first answer but it's wrong

--- advent/src/main/kotlin/site/kevinb9n/advent2022/Day02.kt
+++ advent/src/main/kotlin/site/kevinb9n/advent2022/Day02.kt
@@ -1,17 +1,33 @@
 package site.kevinb9n.advent2022
 
-import com.google.common.collect.Comparators.greatest
-import com.google.common.collect.Streams
-import java.util.Comparator.naturalOrder
-import java.util.stream.Stream
-import kotlin.math.max
+import site.kevinb9n.advent2022.Day02.Shape.PAPER
+import site.kevinb9n.advent2022.Day02.Shape.ROCK
+import site.kevinb9n.advent2022.Day02.Shape.SCISSORS
 
 object Day02 {
   fun part1(real: Boolean): Long {
     var sum = 0L
     Loader.load(2, real).forEachLine {
-      sum += it.toLong()
+      val mine = letterToShape(it[0])
+      val theirs = letterToShape(it[2])
+      val outcome = mine.outcomeAgainst(theirs)
+      sum += mine.points + outcome.points
     }
     return sum
   }
+
+  enum class Shape(val points: Int) { ROCK(1), PAPER(2), SCISSORS(3) }
+  enum class Outcome(val points: Int) { LOSE(0), DRAW(3), WIN(6) }
+
+  fun Shape.outcomeAgainst(other: Shape): Outcome {
+    return Outcome.values()[(ordinal - other.ordinal + 1).mod(3)]
+  }
+
+  fun letterToShape(letter: Char) =
+    when (letter) {
+      'A', 'X' -> ROCK
+      'B', 'Y' -> PAPER
+      'C', 'Z' -> SCISSORS
+      else -> error("")
+    }
 }
--- advent/src/test/kotlin/site/kevinb9n/advent2022/Day02Test.kt
+++ advent/src/test/kotlin/site/kevinb9n/advent2022/Day02Test.kt
@@ -2,21 +2,32 @@ package site.kevinb9n.advent2022
 
 import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.api.Test
+import site.kevinb9n.advent2022.Day02.Outcome.DRAW
+import site.kevinb9n.advent2022.Day02.Outcome.LOSE
+import site.kevinb9n.advent2022.Day02.Outcome.WIN
+import site.kevinb9n.advent2022.Day02.Shape.PAPER
+import site.kevinb9n.advent2022.Day02.Shape.ROCK
+import site.kevinb9n.advent2022.Day02.Shape.SCISSORS
+import site.kevinb9n.advent2022.Day02.outcomeAgainst
 
 class Day02Test {
-  @Test fun part1Fake() {
-    assertThat(Day02.part1(false)).isEqualTo(42L)
+  @Test fun theGame() {
+    assertThat(ROCK.outcomeAgainst(ROCK)).isEqualTo(DRAW)
+    assertThat(ROCK.outcomeAgainst(PAPER)).isEqualTo(LOSE)
+    assertThat(ROCK.outcomeAgainst(SCISSORS)).isEqualTo(WIN)
+    assertThat(PAPER.outcomeAgainst(ROCK)).isEqualTo(WIN)
+    assertThat(PAPER.outcomeAgainst(PAPER)).isEqualTo(DRAW)
+    assertThat(PAPER.outcomeAgainst(SCISSORS)).isEqualTo(LOSE)
+    assertThat(SCISSORS.outcomeAgainst(ROCK)).isEqualTo(LOSE)
+    assertThat(SCISSORS.outcomeAgainst(PAPER)).isEqualTo(WIN)
+    assertThat(SCISSORS.outcomeAgainst(SCISSORS)).isEqualTo(DRAW)
   }
-  //
-  //@Test fun part1Real() {
-  //  assertThat(Day01.part1(true)).isEqualTo(70509L)
-  //}
-  //
-  //@Test fun part2Fake() {
-  //  assertThat(Day01.part2(false)).isEqualTo(45000L)
-  //}
-  //
-  //@Test fun part2Real() {
-  //  assertThat(Day01.part2(true)).isEqualTo(208567L)
-  //}
+
+  @Test fun part1Fake() {
+    assertThat(Day02.part1(false)).isEqualTo(15L)
+  }
+
+  @Test fun part1Real() {
+    assertThat(Day02.part1(true)).isEqualTo(13439L) // but this is wrong
+  }
 }
