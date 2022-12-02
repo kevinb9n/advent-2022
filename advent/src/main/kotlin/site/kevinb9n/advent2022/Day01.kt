aca2794c96efe485acf1bc61721334e99375f813
kevinb9n <kevinb9n@gmail.com>
2022-12-01 17:51:59 -0800

    jeez there's a part 2

--- advent/src/main/kotlin/site/kevinb9n/advent2022/Day01.kt
+++ advent/src/main/kotlin/site/kevinb9n/advent2022/Day01.kt
@@ -1,9 +1,13 @@
 package site.kevinb9n.advent2022
 
+import com.google.common.collect.Comparators.greatest
+import com.google.common.collect.Streams
+import java.util.Comparator.naturalOrder
+import java.util.stream.Stream
 import kotlin.math.max
 
 object Day01 {
-  fun answer(real: Boolean): Long {
+  fun part1(real: Boolean): Long {
     var total = 0L
     var highest = 0L
     Loader.load(1, real).forEachLine {
@@ -15,5 +19,23 @@ object Day01 {
       }
     }
     return highest
+  }
+
+  private val GREATEST_3_COLLECTOR = greatest<Long>(3, naturalOrder())
+
+  fun part2(real: Boolean): Long {
+    var total = 0L
+    val list = mutableListOf<Long>()
+    val source = Loader.load(1, real)
+    Streams.concat(source.lines(), Stream.of("")).forEach {
+      if (it.isNotEmpty()) {
+        total += it.toLong()
+      } else {
+        list.add(total)
+        total = 0
+      }
+    }
+    val greatest3: List<Long> = list.stream().collect(GREATEST_3_COLLECTOR)
+    return greatest3.stream().mapToLong { it }.sum()
   }
 }
--- advent/src/test/kotlin/site/kevinb9n/advent2022/Day01Test.kt
+++ advent/src/test/kotlin/site/kevinb9n/advent2022/Day01Test.kt
@@ -2,14 +2,21 @@ package site.kevinb9n.advent2022
 
 import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.api.Test
-import kotlin.streams.toList
 
 class Day01Test {
-  @Test fun testFake() {
-    assertThat(Day01.answer(false)).isEqualTo(24000L)
+  @Test fun part1Fake() {
+    assertThat(Day01.part1(false)).isEqualTo(24000L)
   }
 
-  @Test fun testReal() {
-    assertThat(Day01.answer(true)).isEqualTo(70509L)
+  @Test fun part1Real() {
+    assertThat(Day01.part1(true)).isEqualTo(70509L)
+  }
+
+  @Test fun part2Fake() {
+    assertThat(Day01.part2(false)).isEqualTo(45000L)
+  }
+
+  @Test fun part2Real() {
+    assertThat(Day01.part2(true)).isEqualTo(208567L)
   }
 }
