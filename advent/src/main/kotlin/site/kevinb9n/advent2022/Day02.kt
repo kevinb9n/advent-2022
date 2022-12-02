4b6e2a64def67e391e22fe477e570536b419cdf6
kevinb9n <kevinb9n@gmail.com>
2022-12-01 21:49:33 -0800

    day 2 part 2

--- advent/src/main/kotlin/site/kevinb9n/advent2022/Day02.kt
+++ advent/src/main/kotlin/site/kevinb9n/advent2022/Day02.kt
@@ -1,5 +1,8 @@
 package site.kevinb9n.advent2022
 
+import site.kevinb9n.advent2022.Day02.Outcome.DRAW
+import site.kevinb9n.advent2022.Day02.Outcome.LOSE
+import site.kevinb9n.advent2022.Day02.Outcome.WIN
 import site.kevinb9n.advent2022.Day02.Shape.PAPER
 import site.kevinb9n.advent2022.Day02.Shape.ROCK
 import site.kevinb9n.advent2022.Day02.Shape.SCISSORS
@@ -18,11 +21,28 @@ object Day02 {
     return sum
   }
 
+  fun part2(real: Boolean): Long {
+    var sum = 0L
+    Loader.load(2, real).forEachLine {
+      val theirs = letterToShape(it[0])
+      val outcome = letterToOutcome(it[2])
+      val mine = outcome.shapeToPlayAgainst(theirs)
+      val points = mine.points + outcome.points
+      // println("$it $points")
+      sum += points
+    }
+    return sum
+  }
+
   enum class Shape(val points: Int) { ROCK(1), PAPER(2), SCISSORS(3) }
   enum class Outcome(val points: Int) { LOSE(0), DRAW(3), WIN(6) }
 
   fun Shape.outcomeAgainst(other: Shape): Outcome {
     return Outcome.values()[(ordinal - other.ordinal + 1).mod(3)]
+  }
+
+  fun Outcome.shapeToPlayAgainst(opponentPlay: Shape): Shape {
+    return Shape.values()[(ordinal + opponentPlay.ordinal - 1).mod(3)]
   }
 
   fun letterToShape(letter: Char) =
@@ -32,4 +52,12 @@ object Day02 {
       'C', 'Z' -> SCISSORS
       else -> error("")
     }
+
+  fun letterToOutcome(letter: Char) =
+      when (letter) {
+        'X' -> LOSE
+        'Y' -> DRAW
+        'Z' -> WIN
+        else -> error("")
+      }
 }
--- advent/src/test/kotlin/site/kevinb9n/advent2022/Day02Test.kt
+++ advent/src/test/kotlin/site/kevinb9n/advent2022/Day02Test.kt
@@ -9,6 +9,7 @@ import site.kevinb9n.advent2022.Day02.Shape.PAPER
 import site.kevinb9n.advent2022.Day02.Shape.ROCK
 import site.kevinb9n.advent2022.Day02.Shape.SCISSORS
 import site.kevinb9n.advent2022.Day02.outcomeAgainst
+import site.kevinb9n.advent2022.Day02.shapeToPlayAgainst
 
 class Day02Test {
   @Test fun theGame() {
@@ -23,11 +24,31 @@ class Day02Test {
     assertThat(SCISSORS.outcomeAgainst(SCISSORS)).isEqualTo(DRAW)
   }
 
+  @Test fun theGame2() {
+    assertThat(DRAW.shapeToPlayAgainst(ROCK)).isEqualTo(ROCK)
+    assertThat(LOSE.shapeToPlayAgainst(PAPER)).isEqualTo(ROCK)
+    assertThat(WIN.shapeToPlayAgainst(SCISSORS)).isEqualTo(ROCK)
+    assertThat(WIN.shapeToPlayAgainst(ROCK)).isEqualTo(PAPER)
+    assertThat(DRAW.shapeToPlayAgainst(PAPER)).isEqualTo(PAPER)
+    assertThat(LOSE.shapeToPlayAgainst(SCISSORS)).isEqualTo(PAPER)
+    assertThat(LOSE.shapeToPlayAgainst(ROCK)).isEqualTo(SCISSORS)
+    assertThat(WIN.shapeToPlayAgainst(PAPER)).isEqualTo(SCISSORS)
+    assertThat(DRAW.shapeToPlayAgainst(SCISSORS)).isEqualTo(SCISSORS)
+  }
+
   @Test fun part1Fake() {
     assertThat(Day02.part1(false)).isEqualTo(15L)
   }
 
   @Test fun part1Real() {
     assertThat(Day02.part1(true)).isEqualTo(11603L)
+  }
+
+  @Test fun part2Fake() {
+    assertThat(Day02.part2(false)).isEqualTo(12L)
+  }
+
+  @Test fun part2Real() {
+    assertThat(Day02.part2(true)).isEqualTo(12725L)
   }
 }
